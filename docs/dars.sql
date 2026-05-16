5c6fd153fdb901fe61b4ed84c346235fe6cb0f68
5kr1mp <skarlbitang@gmail.com>
2026-05-16 14:23:25 +0800

    doc/fix : syntax error

--- docs/dars.sql
+++ docs/dars.sql
@@ -865,7 +865,7 @@ BEGIN
         s.first_name,
         s.last_name,
         b.id,
-        b.barangay_name,
+        b.barangay_name
     FROM staff as s
     INNER JOIN barangay as b ON s.barangay_id = b.id 
     WHERE s.email = p_email
